21946d1db616b4ed97246704db6e421d801cee9d
Imobzy Dev <contato@imobzy.com.br>
2026-04-18 23:53:00 -0300

    fix: allow PATCH method in CORS for CRM updates

--- server/index.js
+++ server/index.js
@@ -64,7 +64,7 @@ app.use(cors({
     callback(new Error(`CORS: Origem não permitida - ${origin}`));
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-impersonate-org-id'],
 }));
 

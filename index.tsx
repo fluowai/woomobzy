41de7addef7c85d42eca876406e66c09f25f1da1
Imobzy Dev <contato@imobzy.com.br>
2026-06-10 18:32:41 -0300

    Fix OKA public site service worker cache

--- index.tsx
+++ index.tsx
@@ -16,15 +16,25 @@ logger.info('Index.tsx: Creating root...');
 const root = ReactDOM.createRoot(rootElement);
 
 logger.info('Index.tsx: Rendering App...');
-const updateSW = registerSW({
-  immediate: true,
-  onNeedRefresh() {
-    updateSW(true);
-  },
-  onOfflineReady() {
-    logger.info('PWA pronta para uso offline.');
-  },
-});
+const publicSiteHosts = new Set(['okaimoveis.com.br', 'www.okaimoveis.com.br']);
+const isPublicSiteHost = typeof window !== 'undefined' && publicSiteHosts.has(window.location.hostname);
+
+if (isPublicSiteHost && 'serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .getRegistrations()
+    .then((registrations) => Promise.all(registrations.map((registration) => registration.unregister())))
+    .catch((error) => logger.warn('Falha ao remover service worker do site público:', error));
+} else {
+  const updateSW = registerSW({
+    immediate: true,
+    onNeedRefresh() {
+      updateSW(true);
+    },
+    onOfflineReady() {
+      logger.info('PWA pronta para uso offline.');
+    },
+  });
+}
 
 root.render(
   <React.StrictMode>
